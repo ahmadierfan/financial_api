a172b14f68fa2744cd78ee076e9ad852e68eb74b
Erfan <ahmadierfan.it@gmail.com>
2025-11-22 14:44:33 +0330

    resolve problem in pre commit for codding table

--- database/migrations/2025_02_21_151850_create_m_warehousedocs_table.php
+++ database/migrations/2025_02_21_151850_create_m_warehousedocs_table.php
@@ -8,14 +8,64 @@ class CreateMWarehousedocsTable extends Migration
 {
     public function up()
     {
+        Schema::create('m_warehousedocs', function (Blueprint $table) {
 
 
+            $table->charset = 'utf8mb4';
 
 
+            $table->collation = 'utf8mb4_persian_ci';
 
 
+            $table->bigIncrements('pk_warehousedoc');
 
 
+            $table->foreignId('fk_registrar')->constrained('users');
+
+
+            $table->foreignId('fk_warehousedoctype')->constrained('b_warehousedoctypes', 'pk_warehousedoctype');
+
+
+            $table->foreignId('fk_warehouse')->constrained('m_warehouses', 'pk_warehouse');
+
+
+            $table->foreignId('fk_destinationwarehouse')->nullable()->constrained('m_warehouses', 'pk_warehouse');
+
+
+            $table->foreignId('fk_system')->nullable()->constrained('b_systems', 'pk_system');
+
+
+            $table->foreignId('fk_invoice')->nullable()->constrained('m_invoices', 'pk_invoice');
+
+
+            $table->foreignId('fk_deliverorrecipient')->nullable()->constrained('users');
+
+
+            $table->date('warehousedocdate');
+
+
+            $table->integer('warehousedoccode')->nullable();
+
+
+            $table->string('deliverorrecipient', 45)->nullable();
+
+
+            $table->integer('shipping')->nullable();
+
+
+            $table->string('description', 145)->nullable();
+
+
+            $table->string('attachments', 145)->nullable();
+
+
+            $table->tinyInteger('isenable')->default(1);
+
+
+            $table->timestamps();  // این خط کافی است
+
+
+        });
 
     }
 
